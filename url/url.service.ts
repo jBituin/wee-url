b3f7e58e37971048499a0bde5759d42e3b8e1d0e
Joemel <bituinjoemel@gmail.com>
2021-02-19 00:05:01 +0800

    [WIP] Add url service

---
+++ url/url.service.ts
@@ -0,0 +1,37 @@
+import UrlModel from './url.model';
+import IUrl from './url.interface';
+import { ERRORS } from '../errors';
+import { nanoid } from 'nanoid';
+import validateUrlDTO from './url.validator';
+
+export default {
+  async getUrl(id: string) {
+    try {
+      const url = await UrlModel.findOne({ slug: id });
+      if (!url) {
+        throw new Error(ERRORS.URL_NOT_FOUND);
+      }
+      return url;
+    } catch (error) {
+      throw new Error(ERRORS.URL_NOT_FOUND);
+    }
+  },
+
+  async createTinyUrl(urlDTO: IUrl) {
+    let { slug, url } = urlDTO;
+    await validateUrlDTO(urlDTO);
+    try {
+      if (!slug) {
+        slug = nanoid(6);
+      } else {
+        const isExisting = await UrlModel.findOne({ slug });
+        if (isExisting) {
+          throw new Error(ERRORS.SLUG_IN_USE);
+        }
+      }
+      slug = slug.toLowerCase();
+      const newUrl = await UrlModel.insert({ slug, url });
+      return newUrl;
+    } catch (error) {}
+  },
+};
