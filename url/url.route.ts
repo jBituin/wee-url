e35a25a0442ce11b43f8823413ff6f99aabb0d86
Joemel <bituinjoemel@gmail.com>
2021-02-19 23:58:59 +0800

    Add rate-limit

--- url/url.route.ts
+++ url/url.route.ts
@@ -1,10 +1,18 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import slowDown from 'express-slow-down';
+import rateLimit from 'express-rate-limit';
+
 import urlController from './url.controller';
 
 const router = express.Router();
 
 router.get('/', urlController.helloWorld);
-router.post('/', urlController.createTinyUrl);
+router.post(
+  '/',
+  slowDown({ windowMs: 10 * 1000, delayAfter: 1, delayMs: 500 }),
+  rateLimit({ windowMs: 10 * 1000, max: 1 }),
+  urlController.createTinyUrl
+);
 router.get('/:id', urlController.getUrl);
 
 export default router;
