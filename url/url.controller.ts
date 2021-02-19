16ef141a8731d15d9dfa465136b33f48c18d4c92
Joemel <bituinjoemel@gmail.com>
2021-02-19 23:01:03 +0800

    Add service integration

--- url/url.controller.ts
+++ url/url.controller.ts
@@ -1,13 +1,26 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
+import UrlService from './url.service';
 
 export default {
   helloWorld(req: Request, res: Response) {
     res.send('Hello World!');
   },
-  async getUrl(req: Request, res: Response) {
-    res.send('@GET url/:id');
+  async getUrl(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id } = req.params;
+      const result = await UrlService.getUrl(id);
+      res.redirect(result);
+    } catch (error) {
+      next(error);
+    }
   },
-  async createTinyUrl(req: Request, res: Response) {
-    res.send('@POST url');
+  async createTinyUrl(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { slug, url } = req.body;
+      const result = await UrlService.createTinyUrl({ slug, url });
+      res.send(result);
+    } catch (error) {
+      next(error);
+    }
   },
 };
