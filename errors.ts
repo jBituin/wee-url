ac9f2738442995032d93e2efb94227f014c3314e
Joemel <bituinjoemel@gmail.com>
2021-02-21 23:57:10 +0800

    Add invalid url error

--- errors.ts
+++ errors.ts
@@ -1,4 +1,5 @@
 export enum ERRORS {
-  'URL_NOT_FOUND' = 'URL_NOT_FOUND',
-  'SLUG_IN_USE' = 'SLUG_IN_USE',
+  'URL_NOT_FOUND' = 'Url not found',
+  'SLUG_IN_USE' = 'Slug in use',
+  'INVALID_URL' = 'Invalid URL',
 }
