1879e9fd99cb81359d76131ca9d7d0da15bd9a54
Joemel <bituinjoemel@gmail.com>
2021-02-26 22:39:20 +0800

    Remove wrapper

--- url/url.model.ts
+++ url/url.model.ts
@@ -1,16 +1,7 @@
 import { db as dbConnection } from '../db';
-import { FilterQuery } from 'mongodb';
 
 const url = dbConnection.get('urls');
+
 url.createIndex({ slug: 1 }, { unique: true });
-export default {
-  ...url,
 
-  findOne(args: FilterQuery<any> | undefined) {
-    return url.findOne(args);
-  },
-
-  insert(args: any) {
-    return url.insert(args);
-  },
-};
+export default url;
